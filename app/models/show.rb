989f7206a5de39b6ee16ad77d2ac4c576839defa
alex lieberman <alex.lieberman182@gmail.com>
2023-04-04 10:53:38 -0700

    commit tree 385d77ed068d0e9d71be76ebc9bf81e7a318790e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+    
+    def #network
+        self.network
+    end
+
+    def actors_list
+        self.actors.map { |item| "#{item.first_name} #{item.last_name}"}
+    end
 end
